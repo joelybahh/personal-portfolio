b60d4451e5bbb79dfdb4f27f0081f7ea9d36c9e7
Joel Gabriel <joel@inspacexr.com>
2024-02-05 20:47:11 +1100

    fix: 🐛 fixed speed insights implementation for HTML  [no-key]
    - Added a script in index.html to initialize Speed Insights
    - Removed main.js file which was previously used to inject Speed Insights
    - Updated package.json dependencies, removed @vercel/speed-insights

--- main.js
+++
@@ -1,5 +0,0 @@
-import { injectSpeedInsights } from "@vercel/speed-insights";
-
-console.log("Hi I'm the main.js file!");
-injectSpeedInsights();
-console.log("I've injected Speed Insights into the main.js file!");
